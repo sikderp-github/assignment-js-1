421a02d2c3628d258ee37402834148792306ec1a
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-07-16 23:04:55 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,35 @@
+// Feet to mile converter
+
+function feetToMile(feet) {
+    let mile = feet / 5280;
+    if (feet == "- ") {
+        alert('Feet value must be positive. Please change the value');
+    }
+    return mile.toFixed(6); // miles fixed to 6 digit 
+}
+var x = feetToMile(1);
+console.log(x);
+
+
+// Wood Calculator 
+
+function woodCalculator() {
+
+
+}
+
+
+// Brick Calculator
+
+function brickCalculator(pcs) {
+
+
+}
+
+
+// Search tiny/junior friends
+
+function tinyFriends() {
+
+
+}
